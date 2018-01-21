d7ec9531ff28e1f0109c47c5b16662ad7ae05a67
Yarden Hochman <yyyarden@gmail.com>
2018-01-21 16:03:13 -0500

    header disappear on landscape

--- client/src/Components/UI/Header/Popup/popup.js
+++ client/src/Components/UI/Header/Popup/popup.js
@@ -3,6 +3,7 @@ import { Menu, Modal } from 'semantic-ui-react';
 
 import RegisterForm from './Register/register';
 import LoginForm from './Login/login';
+import Media from 'react-media';
 
 class Popup extends Component {
   state = { form: 'register' };
@@ -16,14 +17,16 @@ class Popup extends Component {
     else return <RegisterForm login={this.changeForm} />;
   };
   render = () => {
+    let popupStyle = { height: '400px' };
     console.log('Popup updated');
     return (
       <Modal
         dimmer="blurring"
+        style={popupStyle}
         className="Popup"
         size="tiny"
         trigger={
-          <Menu.Item >
+          <Menu.Item>
             <i className="fa fa-user-circle fa-lg" aria-hidden="true" />
           </Menu.Item>
         }

bd59ffe234d16e390eb780b34d80d42d9c7b4f95
Yarden Hochman <yyyarden@gmail.com>
2018-02-07 14:52:40 -0500

    fixed fonts

--- client/src/Components/MainDisplay/Navigation/navigation.js
+++ client/src/Components/MainDisplay/Navigation/navigation.js
@@ -1,37 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
 
 const Navigation = ({next,prev}) => {
-  const LeftButton = styled.a`
-    height: 6vh;
-    gridarea: leftArrow;
-    cursor: pointer;
-  `;
-  const RightButton = styled.a`
-    height: 6vh;
-    gridarea: rightArrow;
-    cursor: pointer;
-  `;
+
   const buttonInnerStyle = {
-    margin: `0 18vw`,
+    margin: `0 20vw`,
     cursor: `pointer`,
   };
-  const Control = styled.div`
-    position: absolute;
-    z-index: 1;
-    width: 100%;
-    top: 80vh;
-  `;
 
   return (
-    <Control>
-      <LeftButton onClick={prev}>
+    <div className='control'>
+      <a className='left_button' onClick={prev}>
         <i style={buttonInnerStyle} className="fa fa-arrow-left fa-4x " />
-      </LeftButton>
-      <RightButton onClick={next}>
+      </a>
+      <a className='right_button' onClick={next}>
         <i style={buttonInnerStyle} className="fa fa-arrow-right fa-4x " />
-      </RightButton>
-    </Control>
+      </a>
+    </div>
   );
 };
 

c9feb8d32a23e5d581846b6dac6490054db53174
Yarden Hochman <yyyarden@gmail.com>
2018-02-10 21:34:00 -0500

    changed image address to https

--- client/src/Components/UI/Credits/about.js
+++ client/src/Components/UI/Credits/about.js
@@ -69,7 +69,7 @@ var style = styler`
 var yarden = {
   name: 'Yarden Hochman',
   img:
-    'http://res.cloudinary.com/dz2nxhscn/image/upload/v1518158450/16832212_1326016857444631_3977183095561215084_n_osizmh.jpg',
+    'https://res.cloudinary.com/dz2nxhscn/image/upload/v1518158450/16832212_1326016857444631_3977183095561215084_n_osizmh.jpg',
   profileSite: 'https://polar-tundra-13345.herokuapp.com/',
   text:
     'Yarden is a Full Stack Engineer currently looking for opportunities in Colorado or California. His favorite fruit is Mango.',
@@ -77,7 +77,7 @@ var yarden = {
 var Anatoliy = {
   name: 'Anatoliy Zaslavskiy',
   img:
-    'http://res.cloudinary.com/dz2nxhscn/image/upload/v1518127206/20046539_10156211418873265_263186605151505675_n_ah72ly.jpg',
+    'https://res.cloudinary.com/dz2nxhscn/image/upload/v1518127206/20046539_10156211418873265_263186605151505675_n_ah72ly.jpg',
   profileSite: 'https://www.linkedin.com/in/tolicodes',
   text:
     'Toli is a frontend programmer working at Hover Inc in SF, California. He loves everything active including acro yoga, hiking, jogging, skiing, and PsyTrance parties.',

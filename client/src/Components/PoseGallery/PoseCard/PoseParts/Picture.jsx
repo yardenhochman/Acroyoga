05d4a3b054d7ac9656df423f230270a85e9e8de5
yarden hochman <yyyarden@gmail.com>
2018-02-21 23:16:38 -0500

    readding resizing since issue presists

--- client/src/Components/PoseGallery/PoseCard/PoseParts/Picture.jsx
+++ client/src/Components/PoseGallery/PoseCard/PoseParts/Picture.jsx
@@ -13,12 +13,18 @@ import VisibilitySensor from 'react-visibility-sensor';
 const PosePicture = ({ img }) => (
 	<StyleRoot>
 		<VisibilitySensor>
-			<img
-				src={img}
-				style={style.image}
-				alt={'Loading...'}
-				loader={LoadDisplay}
-			/>
+			<picture>
+				<source
+					srcSet={img.replace(/(?:upload).+\//, 'upload/w_2000/')}
+					media={Desktop}
+				/>
+				<img
+					src={img.replace(/(?:upload).+\//, 'upload/w_1000/')}
+					style={style.image}
+					alt={'Loading...'}
+					loader={LoadDisplay}
+				/>
+			</picture>
 		</VisibilitySensor>
 	</StyleRoot>
 );
